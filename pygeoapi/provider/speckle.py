cf52c511a8beb72eaa7ce060ee89f5e5ea97fa21
KatKatKateryna <kateryna@speckle.systems>
2024-07-07 19:23:20 +0100

    ex

--- pygeoapi/provider/speckle.py
+++ pygeoapi/provider/speckle.py
@@ -77,9 +77,8 @@ class SpeckleProvider(BaseProvider):
             print("Installing Speckle dependencies")
 
             from subprocess import run
-            import logging
 
-            logging.info(self.get_python_path())
+            raise Exception(self.get_python_path())
             completed_process = run(
                 [
                     self.get_python_path(),
